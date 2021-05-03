db00182c90449d10daf16a7312922bc0062a7b14
AdfDeveloper <AdfDeveloper@192.168.2.2>
2021-05-03 11:38:22 +0500

    DB To Local

--- ViewController/src/view/DatabaseConnection/DatabaseConnection.java
+++ ViewController/src/view/DatabaseConnection/DatabaseConnection.java
@@ -24,7 +24,7 @@ public class DatabaseConnection {
         /* Database credentials >> username , password , hostname , port , SID  */
         setUNamee("lihs");
         setUpasss("lihsir");
-        setUipp("62.171.154.170");
+        setUipp("192.168.1.4");
         setUportADF("1521");
         setUSID("orcl");
         /* END  */
