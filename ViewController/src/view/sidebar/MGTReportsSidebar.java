7ac03fa105cdff3965252f55a879ea0f43297fc8
ReportServer <ReportServer@192.168.2.4>
2021-06-29 10:06:01 +0500

    Add Page in MGT module

--- ViewController/src/view/sidebar/MGTReportsSidebar.java
+++ ViewController/src/view/sidebar/MGTReportsSidebar.java
@@ -7,21 +7,14 @@ import javax.faces.event.ActionEvent;
 import oracle.adf.controller.TaskFlowId;
 
 public class MGTReportsSidebar implements Serializable {
-    private String taskFlowId = "/WEB-INF/Task_Flows/Modules/MGT_Reports/MGT_Reports_TaskFlow.xml#MGT_Reports_TaskFlow";
-    private String taskFlowId1 =
-        "/WEB-INF/Task_Flows/Modules/MGT_Reports/MGT_Reports_TaskFlow.xml#MGT_Reports_TaskFlow";
 
+
+    private String taskFlowId = "/WEB-INF/Task_Flows/Modules/MGT_Reports/MGT_Reports_TaskFlow.xml#MGT_Reports_TaskFlow";
 
     public MGTReportsSidebar() {
     }
 
-    public TaskFlowId getDynamicTaskFlowId() {
-        return TaskFlowId.parse(taskFlowId);
-    }
-
-    public void setDynamicTaskFlowId(String taskFlowId) {
-        this.taskFlowId = taskFlowId;
-    }
+   
     
     public void fragmentCall(ActionEvent actionEvent) {
         // Add event code here...
@@ -34,11 +27,11 @@ public class MGTReportsSidebar implements Serializable {
         setDynamicTaskFlowId(link.toString());
     }
 
-    public TaskFlowId getDynamicTaskFlowId1() {
-        return TaskFlowId.parse(taskFlowId1);
+    public TaskFlowId getDynamicTaskFlowId() {
+        return TaskFlowId.parse(taskFlowId);
     }
 
-    public void setDynamicTaskFlowId1(String taskFlowId) {
-        this.taskFlowId1 = taskFlowId;
+    public void setDynamicTaskFlowId(String taskFlowId) {
+        this.taskFlowId = taskFlowId;
     }
 }
